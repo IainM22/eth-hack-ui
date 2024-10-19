77d9043967b906965c9fcf18225c6fb473e7b67e
Iain McCown <iain@edgeandnode.com>
2024-10-19 00:28:50 -0700

    basic config with wagmi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { useAccount } from "wagmi";
 import { SimpleTx } from "./SimpleTx";
 
 export default function HomePage() {
+  const { address } = useAccount();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
@@ -11,6 +15,7 @@ export default function HomePage() {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <DynamicWidget variant="dropdown" />
           <SimpleTx />
+          <p>{address}</p>
         </div>
       </div>
     </main>
